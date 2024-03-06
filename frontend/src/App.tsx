bc6dbd50f56f1a8d9e422dcc8995bd5891c04fd3
Martin Hermes <okapiposter@gmail.com>
2024-03-06 18:22:05 +0100

    edit app.tsx, add ProductList.tsx, edit addProduct

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import {Route, Routes} from "react-router-dom";
 import ProductUpdate from "./components/ProductUpdate.tsx";
 import AddProductCard from "./components/AddProductCard.tsx";
 import ProductDetail from "./components/ProductDetail";
+import {ProductList} from "./components/ProductList.tsx";
 
 export default function App() {
 
     return (
         <main>
             <Routes>
-                <Route path={"/"} element={<h2>Hello World </h2>}/>
+                <Route path={"/"} element={<ProductList />} />
                 <Route path={"/products/add"} element={<AddProductCard />}/>
                 <Route path={"/product/:id"} element={<ProductUpdate />}/>
                 <Route path={"/products/:id"} element={<ProductDetail/>}/>
