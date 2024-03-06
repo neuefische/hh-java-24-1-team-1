4a72bf96bcef97c2aa18acb97ff8ec8ad264b54e
Khorm247 <44656481+Khorm247@users.noreply.github.com>
2024-03-06 11:50:05 +0100

    add findById() to ProductRepo update getProductById to ProductService

--- backend/src/main/java/com/github/hh/backend/repository/ProductRepo.java
+++ backend/src/main/java/com/github/hh/backend/repository/ProductRepo.java
@@ -4,6 +4,9 @@ import com.github.hh.backend.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepo extends MongoRepository<Product,String> {
+    Optional<Product> findById(String id);
 }
