7011cfa8806cf262aa2c7f7f00c07e34617424cd
mahambach <ma.hambach@gmx.net>
2024-03-06 18:05:16 +0100

    Rebased product onto this.

--- backend/src/test/java/com/github/hh/backend/controller/ProductControllerTest.java
+++ backend/src/test/java/com/github/hh/backend/controller/ProductControllerTest.java
@@ -1,37 +1,42 @@
 package com.github.hh.backend.controller;
 
 import com.github.hh.backend.model.Product;
-import com.github.hh.backend.repository.ProductRepo;
+import com.github.hh.backend.model.ProductDTO;
+import com.github.hh.backend.service.ProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.mockito.Mockito.*;
+
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ProductControllerTest {
-    @Autowired
-    private ProductRepo productRepo;
 
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private ProductService mockProductService;
 
     @Test
     void updateProduct_shouldReturnUpdatedProduct() throws Exception {
         // Given
-        Product product = new Product("1", "Product", 10,"Description");
-        productRepo.save(product);
+        Product product = new Product("1", "Updated Product", 5,"Updated Description");
+        when(mockProductService.updateProduct(product)).thenReturn(product);
 
         // When and Then
         mvc.perform(MockMvcRequestBuilders.put("/api/products/update")
                 .contentType(MediaType.APPLICATION_JSON)
-                        .content(
-                                """
+                        .content("""
                                 {
                                   "id" : "1",
                                   "name" : "Updated Product",
@@ -41,18 +46,55 @@ class ProductControllerTest {
                                 """
                         ))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json(
-                        """
+                .andExpect(MockMvcResultMatchers.content().json("""
                         {
                           "id" : "1",
                           "name" : "Updated Product",
                           "amount" : 5,
-                            "description" : "Updated Description"
+                          "description" : "Updated Description"
                         }
                         """
                 ));
+
+        verify(mockProductService, times(1)).updateProduct(product);
+        verifyNoMoreInteractions(mockProductService);
     }
 
+
+    @Test
+    void addProduct_whenNewProductDTOGiven_thenReturnProductIncludingNewID() throws Exception {
+        // Given
+        ProductDTO productDTO = new ProductDTO("Product", 10,"Description");
+        Product product = new Product("1", "Product", 10,"Description");
+        when(mockProductService.addProduct(productDTO)).thenReturn(product);
+
+        // When and Then
+        mvc.perform(MockMvcRequestBuilders.post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                """
+                                {
+                                  "name" : "Product",
+                                  "amount" : 10,
+                                  "description" : "Description"
+                                }
+                                """
+                        ))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """
+                        {
+                          "id" : "1",
+                          "name" : "Product",
+                          "amount" : 10,
+                            "description" : "Description"
+                        }
+                        """
+                ));
+
+        verify(mockProductService, times(1)).addProduct(productDTO);
+        verifyNoMoreInteractions(mockProductService);
+    }
 
 
 }
