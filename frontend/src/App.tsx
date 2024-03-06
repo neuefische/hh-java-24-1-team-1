9e17bcbae3a131a90deb77a77701e6bf93433636
mahambach <ma.hambach@gmx.net>
2024-03-06 12:42:37 +0100

    remove triggerChange from AddProductCard.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
         <main>
             <Routes>
                 <Route path={"/"} element={<h2>Hello World </h2>}/>
-                <Route path={"/products/add"} element={<AddProductCard triggerChange={fetchData}/>}/>
+                <Route path={"/products/add"} element={<AddProductCard />}/>
             </Routes>
         </main>
     )
