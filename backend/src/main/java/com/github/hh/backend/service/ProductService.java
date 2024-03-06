0a48e81d813ad92e45d984b82012dc69243c31e3
mahambach <ma.hambach@gmx.net>
2024-03-06 11:46:03 +0100

    add POST backend

--- backend/src/main/java/com/github/hh/backend/service/ProductService.java
+++ backend/src/main/java/com/github/hh/backend/service/ProductService.java
@@ -1,11 +1,20 @@
 package com.github.hh.backend.service;
 
+import com.github.hh.backend.model.Product;
+import com.github.hh.backend.model.ProductDTO;
 import com.github.hh.backend.repository.ProductRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepo productRepo;
+
+    public Product addProduct(ProductDTO productDTO) {
+        List<Product> products = productRepo.findAll();
+        return productRepo.save(new Product(String.valueOf(products.size() + 1), productDTO.name(), productDTO.amount(), productDTO.description()));
+    }
 }
