b92403d766009ac9f63008f09138877d8df5ed64
Khorm247 <44656481+Khorm247@users.noreply.github.com>
2024-03-06 12:19:45 +0100

    add Route to "/products/:id" in App.tsx add ProductDetail.tsx component

---
+++ frontend/src/components/ProductDetail.tsx
@@ -0,0 +1,10 @@
+export default function ProductDetail() {
+    return <>
+        <div>
+            <h2>Product Detail</h2>
+            <p>Produkt Name</p>
+            <p>Artikelnummer</p>
+            <p>Bestand</p>
+        </div>
+    </>
+}
