3a86580e35aa2384de49772b7e07e444420aa3b9
Ugur Kartal <kartalugur@gmail.com>
2024-03-06 15:24:29 +0100

    add update operation on backend and frontend

--- backend/src/main/java/com/github/hh/backend/controller/ProductController.java
+++ backend/src/main/java/com/github/hh/backend/controller/ProductController.java
@@ -1,18 +1,23 @@
 package com.github.hh.backend.controller;
 
 import com.github.hh.backend.model.Product;
+import com.github.hh.backend.model.Product;
 import com.github.hh.backend.model.ProductDTO;
 import com.github.hh.backend.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
+
+    @PutMapping("/update")
+    public void updateProduct(@RequestBody Product product) {
+        productService.updateProduct(product);
+    }
 
     @PostMapping
     public Product addProduct(ProductDTO productDTO){
