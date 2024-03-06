9641a8b8080b40222746bb46a4b817b4e1a8f621
mahambach <ma.hambach@gmx.net>
2024-03-06 11:02:55 +0100

    add frontend structure.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
+import {Route, Routes} from "react-router-dom";
 
 
 export default function App() {
 
-  return (
-    <>
-        <h1>Hello World</h1>
-    </>
-  )
+    return (
+        <>
+            <main>
+                <Routes>
+                    <Route path={"/"} element={<h2>Hello World </h2>}/>
+                </Routes>
+            </main>
+        </>
+    )
 }
