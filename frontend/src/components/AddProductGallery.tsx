4718b0491a1748cea1a0e705634405128da58587
MaHambach <ma.hambach@gmx.net>
2024-03-07 14:36:36 +0100

    move axios command from AddProductCard to AddProductGallery.tsx.
    add handleSubmit function in AddProductGallery.tsx.

--- frontend/src/components/AddProductGallery.tsx
+++ frontend/src/components/AddProductGallery.tsx
@@ -1,18 +1,29 @@
 import 'AddProductGallery.css'
 import AddProductCard from "./AddProductCard.tsx";
-import {useState} from "react";
+import {FormEvent, useState} from "react";
 import ProductCard from "./ProductCard.tsx";
 import {Product} from "../types/Product.ts";
+import axios from "axios";
+import {ProductDTO} from "../types/ProductDTO.ts";
 
 export default function AddProductGallery():JSX.Element{
-    const [submittedProducts, setSubmittedProducts] = new useState<Product[]>([])
+    const [submittedProducts, setSubmittedProducts] = useState<Product[]>([]);
 
-    function handleSubmit(event){
+    function handleSubmit(event:FormEvent<HTMLFormElement>, newProduct:ProductDTO):void {
+        event.preventDefault();
+        axios.post('../api/products', newProduct)
+            .then(response => {
+                setSubmittedProducts(submittedProducts.concat(response.data));
+                console.log("New product added with id " + response.data.id + ".");
+            })
+            .catch(error => {
+                console.error("Error creating product: ", error.message);
+            })
     }
 
     return (
         <main>
-            <AddProductCard handleSubmit={}/>
+            <AddProductCard handleSubmit={handleSubmit}/>
             {
                 submittedProducts ?
                     submittedProducts.map((product:Product)=> <ProductCard key={product.id} product={product}/>) :
