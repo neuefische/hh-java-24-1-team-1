e358b0d6e2f5a49512ed94f61c3ca9ca9934cd68
steven <steven.mandau@gmx.de>
2024-03-07 12:01:33 +0100

    update ProductDetail.tsx

--- frontend/src/components/ProductDetail.tsx
+++ frontend/src/components/ProductDetail.tsx
@@ -1,16 +1,39 @@
-// import { Link } from 'react-router-dom';
-export default function ProductDetail() {
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {useParams, Link} from "react-router-dom";
+
+export default function ProductDetail(){
+
+    const { id = '' } = useParams<string>();
+    const [product, setProduct] = useState({
+        id: id,
+        name: '',
+        description: '',
+        amount: 0
+    });
+
+    useEffect(() => {
+        axios.get(`/api/products/${id}`)
+            .then(response => {
+                setProduct(response.data);
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }, [id]);
+
     return <>
         <div>
             <h2>Product Detail</h2>
-            <p>Produkt Name</p>
-            <p>Artikelnummer</p>
-            <p>Bestand</p>
-            {/*
+            <p>{product.name}</p>
+            <p>{product.id}</p>
+            <p>{product.amount}</p>
+            <p>{product.description}</p>
+            {
             <Link to={`/products/${id}/edit`}>
                 <button type="button">Update</button>
             </Link>
-            */}
+            }
         </div>
     </>
 }
