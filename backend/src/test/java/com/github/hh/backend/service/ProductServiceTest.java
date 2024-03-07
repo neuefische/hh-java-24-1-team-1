3a271e0df33d31b86790fd2da49937d84c9868c7
MaHambach <ma.hambach@gmx.net>
2024-03-07 14:09:08 +0100

    fix addProduct test in ProductServiceTest
    fix updateProduct test in ProductServiceTest

--- backend/src/test/java/com/github/hh/backend/service/ProductServiceTest.java
+++ backend/src/test/java/com/github/hh/backend/service/ProductServiceTest.java
@@ -4,48 +4,50 @@ import com.github.hh.backend.model.Product;
 import com.github.hh.backend.model.ProductDTO;
 import com.github.hh.backend.repository.ProductRepo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
-
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ProductServiceTest {
-    private final ProductRepo mockProductRepo = mock(ProductRepo.class);
+    @Autowired
+    private ProductRepo mockProductRepo;
 
     @Test
     void addProduct_whenNewProductDTOGiven_thenReturnProductIncludingNewID(){
         // Given
         ProductDTO productDTO = new ProductDTO("Product", 10,"Description");
-        Product expectedOutput = new Product("1", "Product", 10,"Description");
-        Product expectedInput = new Product(null, "Product", 10,"Description");
-        when(mockProductRepo.save(expectedInput)).thenReturn(expectedOutput);
+        Product expected = new Product("1", "Product", 10,"Description");
 
         // When
         ProductService productService = new ProductService(mockProductRepo);
+        Product actual = productService.addProduct(productDTO);
 
         // Then
-        assertEquals(expectedOutput, productService.addProduct(productDTO));
-        verify(mockProductRepo, times(1)).save(expectedInput);
-        verifyNoMoreInteractions(mockProductRepo);
+        assertEquals(expected.name(), actual.name());
+        assertEquals(expected.amount(), actual.amount());
+        assertEquals(expected.description(), actual.description());
+        assertNotNull(actual.id());
     }
 
     @Test
     void updateProduct_shouldReturnUpdatedProduct() {
         // Given
-        Product product = new Product("1", "Product", 5,"Description");
-        when(mockProductRepo.save(product)).thenReturn(product);
+        ProductDTO productDTO = new ProductDTO("Product", 10,"Description");
 
         // When
         ProductService productService = new ProductService(mockProductRepo);
+        Product expected = productService.addProduct(productDTO);
+        Product actual = productService.updateProduct(expected.withAmount(5).withName("Updated Name").withDescription("Updated Description"));
 
+        expected = expected.withAmount(5).withName("Updated Name").withDescription("Updated Description");
         // Then
-        assertEquals(product, productService.updateProduct(product));
-        verify(mockProductRepo, times(1)).save(product);
-        verifyNoMoreInteractions(mockProductRepo);
+        assertEquals(expected, actual);
     }
 
     @Test
