f29823f1675c1c257771db0d47f094b12c6012c5
Martin Hermes <okapiposter@gmail.com>
2024-03-08 21:36:32 +0100

    add basic styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,10 +4,19 @@ import {Link} from "react-router-dom";
 export default function Header(){
 
     return (
-        <header>
+        <header style={{
+            border: '1px solid #ddd',
+            padding: '10px',
+            borderRadius: '5px',
+            margin: '20px 10px',
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            alignItems: 'center',
+            backgroundColor: '#f8f8f8'
+        }}>
             <Link to={"/"}>Home</Link>
-            | <Link to={"/products/add"}>Produkt Hinzufügen</Link>
-            | <Link to={"/critical"}>Kritische Produkte</Link>
+            <Link to={"/products/add"}>Produkt Hinzufügen</Link>
+            <Link to={"/critical"}>Kritische Produkte</Link>
         </header>
     )
 }
