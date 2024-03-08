8efe943a9e6ab222d8fc6971f0b69815f02f6778
steven <steven.mandau@gmx.de>
2024-03-08 15:48:39 +0100

    removed console.log

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -8,7 +8,6 @@ export default function SearchBar(props: Readonly<SearchBarProps>){
 
     function handleSearchText(event: ChangeEvent<HTMLInputElement>){
         props.handleSearchText(event.target.value)
-        console.log(event.target.value)
     }
 
     return(
