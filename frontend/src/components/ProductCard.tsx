f986a0ff63055341efe369853d34441f20036cb6
steven <steven.mandau@gmx.de>
2024-03-08 18:48:54 +0100

    renamed "Details" to "Mehr" (to match the "weniger" Button) add Link to Details Page to ProductCard.tsx

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,27 +1,29 @@
 import React, {useState} from 'react';
 import {Product} from '../types/Product.ts';
-import {NavigateFunction, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 type ProductCardProps = {
     product: Product;
 };
 
 export default function ProductCard({ product }: Readonly<ProductCardProps>): React.ReactElement {
-    const [showDescription, setShowDescription] = useState(false);
-
-    const navigate:NavigateFunction = useNavigate();
+    const [showMore, setShowMore] = useState(false);
 
     const toggleDescription = () => {
-        setShowDescription(!showDescription);
+        setShowMore(!showMore);
     };
 
     return (
         <div style={{ border: '1px solid #ddd', padding: '10px', borderRadius: '5px', margin: '10px 0' }}>
             <h2>{product.name}</h2>
             <p>Menge: {product.amount}</p>
-            {showDescription && <p>Beschreibung: {product.description}</p>}
-            <button onClick={toggleDescription}>{showDescription ? 'Weniger' : 'Mehr'}</button>
-            <button onClick={() => navigate("/products/" + product.id)}>Zu den Details</button>
+            {showMore &&
+                <>
+                    <p>Beschreibung: {product.description}</p>
+                    <p><Link to={"/products/" + product.id}>Details</Link></p>
+                </>
+            }
+            <button onClick={toggleDescription}>{showMore ? 'Weniger' : 'Mehr'}</button>
         </div>
     );
 }
