fb774e7779ae440522af76a83f16eb5382e4d417
MaHambach <ma.hambach@gmx.net>
2024-03-08 16:00:49 +0100

    Refactor all main components to use

--- frontend/src/components/ProductDetail.tsx
+++ frontend/src/components/ProductDetail.tsx
@@ -1,8 +1,8 @@
 import './ProductDetail.css'
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {NavigateFunction, useParams, Link, useNavigate} from "react-router-dom";
 import {RestfulUtility} from "../types/RestfulUtility.ts";
-import axios from "axios";
+import {Product} from "../types/Product.ts";
 
 type ProductDetailProps = {
     restfulUtility:RestfulUtility,
@@ -10,39 +10,30 @@ type ProductDetailProps = {
 
 export default function ProductDetail(props:Readonly<ProductDetailProps>){
     const { id = '' } = useParams<string>();
-    const [product, setProduct] = useState({
-        id: id,
-        name: '',
-        description: '',
-        amount: 0,
-        productNumber: '',
-        minimumStockLevel: 0
-    });
+    const [product] = useState<Product>(props.restfulUtility.getProductById(id));
 
     const navigate:NavigateFunction = useNavigate();
 
-    useEffect(() => setProduct(props.restfulUtility.getProductById(id)), []);
-
-    function handleDeleteProduct(event: MouseEvent):void {
-        event.preventDefault();
+    function handleDeleteProduct():void {
         props.restfulUtility.deleteProductById(id);
         navigate("/");
     }
 
     return (
-        <div className={"details"}>
+        <main>
             <h2>Product Detail</h2>
-            <p>Artikel: {product.name}</p>
-            <p>Menge: {product.amount}</p>
-            <p>Beschreibung: {product.description}</p>
-            <p>Artikelnummer: {product.productNumber}</p>
-            <p>Mindestbestand: {product.minimumStockLevel}</p>
+            <p>{product.name}</p>
+            <p>{product.id}</p>
+            <p>{product.amount}</p>
+            <p>{product.description}</p>
+            <p>{product.productNumber}</p>
+            <p>{product.minimumStockLevel}</p>
             {
                 <Link to={`/products/${id}/edit`}>
                     <button type="button">Update</button>
                 </Link>
             }
             <button className={"deleteButton"} onClick={handleDeleteProduct} type={"button"}>Löschen</button>
-        </div>
+        </main>
     )
 }
