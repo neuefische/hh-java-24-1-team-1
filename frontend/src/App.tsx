e4faa179e5ed45d4faca0472ba7a60ffb5493c9b
Ugur Kartal <kartalugur@gmail.com>
2024-03-09 08:17:19 +0100

    merge ProductList.tsx and CriticalProductList.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,8 @@ import ProductDetail from "./components/ProductDetail";
 import {ProductList} from "./components/ProductList.tsx";
 import Header from "./components/Header.tsx";
 import AddProductGallery from "./components/AddProductGallery.tsx";
-import CriticalProductList from "./components/CriticalProductList.tsx";
 
 export default function App() {
-
     return (
         <>
             <Header />
@@ -17,7 +15,7 @@ export default function App() {
                     <Route path={"/products/add"} element={<AddProductGallery />}/>
                     <Route path={"/products/:id/edit"} element={<ProductUpdate />}/>
                     <Route path={"/products/:id"} element={<ProductDetail/>}/>
-                    <Route path={"/critical"} element={<CriticalProductList />} />
+                    <Route path={"/critical"} element={<ProductList />}/>
                 </Routes>
             </main>
         </>
--- frontend/src/components/ProductList.tsx
+++ frontend/src/components/ProductList.tsx
@@ -4,22 +4,26 @@ import {Product} from '../types/Product.ts';
 import SearchBar from "./SearchBar.tsx";
 import ProductCard from "./ProductCard.tsx";
 import './ProductList.css';
+import {useLocation} from 'react-router-dom';
 
 export function ProductList(): React.ReactElement {
     const [products, setProducts] = useState<Product[]>([]);
     const [searchText, setSearchText] = useState<string>("");
+    const { pathname } = useLocation();
+    const apiUrl = pathname === '/critical' ? '/api/products/critical' : '/api/products';
 
-    function fetchProducts() {
-        axios.get('/api/products')
-            .then(response => {
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get(apiUrl);
                 setProducts(response.data);
-            })
-            .catch(error => {
+            } catch (error) {
                 console.error('Es gab ein Problem beim Abrufen der Produkte:', error);
-            });
-    }
+            }
+        };
 
-    useEffect(fetchProducts, []);
+        fetchProducts();
+    }, [apiUrl]);
 
     const filteredProducts = products.filter(product =>
         product.name.toLowerCase().includes(searchText.toLowerCase())
