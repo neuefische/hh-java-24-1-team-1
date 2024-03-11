e1d4d2a2d3b06c9f48ad180cfe676da6eb95e8d6
steven <steven.mandau@gmx.de>
2024-03-11 18:27:16 +0100

    fixed SearchBar.tsx

--- frontend/src/components/utility/SearchBar.tsx
+++ frontend/src/components/utility/SearchBar.tsx
@@ -1,60 +1,28 @@
 import './SearchBar.css';
-import {ChangeEvent, FormEvent, useEffect, useState} from "react";
+import {ChangeEvent} from "react";
 import {Product} from "../../types/Product.ts";
 
 type SearchBarProps = {
     setResult: (products:Product[]) => void;
     products: Product[];
+    handleSearchText: (searchText:string) => void;
+    handleFilterChange: (filter:string) => void;
 }
 
 export default function SearchBar(props: Readonly<SearchBarProps>):JSX.Element{
-    const [selectedFilter, setSelectedFilter] = useState<string>("all");
-    const [searchText, setSearchText] = useState<string>("");
-    const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    function handleSubmit(event: FormEvent<HTMLFormElement>):void{
-        event.preventDefault();
-    }
 
     function handleSearchText(event: ChangeEvent<HTMLInputElement>):void{
         const searchText = event.target.value.toLowerCase();
-        setSearchText(searchText);
+        props.handleSearchText(searchText);
     }
 
     function handleFilterChange(event: ChangeEvent<HTMLSelectElement>):void{
-        setSelectedFilter(event.target.value);
-    }
-
-    useEffect(() => {
-        setIsLoading(true);
-        props.setResult(
-            props.products.filter(
-                (product:Product) => {
-                    if (selectedFilter === "name") {
-                        return product.name.toLowerCase().includes(searchText);
-                    } else if (selectedFilter === "description") {
-                        return product.description.toLowerCase().includes(searchText);
-                    } else if (selectedFilter === "productNumber"){
-                        return product.productNumber.toLowerCase().includes(searchText);
-                    }
-                    else if (selectedFilter === "all") {
-                        return product.name.toLowerCase().includes(searchText) ||
-                            product.description.toLowerCase().includes(searchText) ||
-                            product.productNumber.toLowerCase().includes(searchText);
-                    }
-                    return false;
-                }
-            )
-        )
-        setIsLoading(false);
-    }, [searchText, selectedFilter, props.products, props]);
-
-    if (isLoading) {
-        return <div>Loading...</div>;
+        props.handleFilterChange(event.target.value);
     }
 
     return (
-        <form onSubmit={handleSubmit} className={"searchBar"}>
+        <div className={"searchBar"}>
             <input onChange={handleSearchText}/>
             <select defaultValue={"all"} onChange={handleFilterChange}>
                 <option value="all">Alle</option>
@@ -62,6 +30,6 @@ export default function SearchBar(props: Readonly<SearchBarProps>):JSX.Element{
                 <option value="description">Beschreibung</option>
                 <option value="productNumber">Artikelnummer</option>
             </select>
-        </form>
+        </div>
     );
 }
