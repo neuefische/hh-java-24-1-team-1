3007003a21a07f8a48beb723d6e8da0700258919
MaHambach <ma.hambach@gmx.net>
2024-03-11 14:42:31 +0100

    rework all tests in ProductServiceTest.java

--- backend/src/test/java/com/github/hh/backend/service/ProductServiceTest.java
+++ backend/src/test/java/com/github/hh/backend/service/ProductServiceTest.java
@@ -5,17 +5,79 @@ import com.github.hh.backend.model.Product;
 import com.github.hh.backend.model.ProductDTO;
 import com.github.hh.backend.repository.ProductRepo;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+
 class ProductServiceTest {
-    @Autowired
-    private ProductRepo mockProductRepo;
+
+    private final ProductRepo mockProductRepo = mock(ProductRepo.class);
+
+    private final ProductService productService = new ProductService(mockProductRepo);
+
+    @Test
+    void findAllProducts_whenEmptyDb_thenReturnEmptyList(){
+        // Given
+        List<Product> expected = List.of();
+        // When
+        when(mockProductRepo.findAll()).thenReturn(expected);
+        List<Product> actual = productService.findAllProducts();
+        // Then
+        assertEquals(expected, actual);
+        verify(mockProductRepo).findAll();
+        verifyNoMoreInteractions(mockProductRepo);
+    }
+
+    @Test
+    void findAllProducts_whenProductsInDb_thenReturnThose() {
+        // Given
+        List<Product> expected = List.of(
+                new Product("1", "Product 1", 10,"Description 1", "1", 5),
+                new Product("2", "Product 2", 20,"Description 2", "2", 10)
+        );
+
+        // When
+        when(mockProductRepo.findAll()).thenReturn(expected);
+        List<Product> actual = productService.findAllProducts();
+
+        // Then
+        assertEquals(expected, actual);
+        verify(mockProductRepo).findAll();
+        verifyNoMoreInteractions(mockProductRepo);
+    }
+
+    @Test
+    void getProductById_shouldReturnProduct() {
+        // Given
+        Product expected = new Product("1", "Product 1", 10,"Description 1", "1", 5);
+
+        // When
+        when(mockProductRepo.existsById(expected.id())).thenReturn(true);
+        when(mockProductRepo.findById(expected.id())).thenReturn(java.util.Optional.of(expected));
+        Product actual = productService.getProductById(expected.id());
+
+        // Then
+        assertEquals(expected, actual);
+        verify(mockProductRepo).existsById(expected.id());
+        verify(mockProductRepo).findById(expected.id());
+        verifyNoMoreInteractions(mockProductRepo);
+    }
+
+    @Test
+    void getProductById_whenNoSuchId_thenThrow() {
+        // Given
+        String id = "1";
+        // When
+        when(mockProductRepo.existsById(id)).thenReturn(false);
+
+        // Then
+        assertThrows(NoSuchProductException.class, () -> productService.getProductById(id));
+        verify(mockProductRepo).existsById(id);
+        verifyNoMoreInteractions(mockProductRepo);
+    }
 
     @Test
     void addProduct_whenNewProductDTOGiven_thenReturnProductIncludingNewID(){
@@ -24,90 +86,88 @@ class ProductServiceTest {
         Product expected = new Product("1", "Product", 10,"Description", "1", 5);
 
         // When
-        ProductService productService = new ProductService(mockProductRepo);
+        when(mockProductRepo.save(any(Product.class))).thenReturn(expected);
         Product actual = productService.addProduct(productDTO);
 
         // Then
-        assertEquals(expected.name(), actual.name());
-        assertEquals(expected.amount(), actual.amount());
-        assertEquals(expected.description(), actual.description());
-        assertNotNull(actual.id());
+        assertEquals(expected, actual);
+        verify(mockProductRepo).save(any(Product.class));
+        verifyNoMoreInteractions(mockProductRepo);
     }
 
     @Test
     void updateProduct_shouldReturnUpdatedProduct() {
         // Given
-        ProductDTO productDTO = new ProductDTO("Product", 10,"Description", "1", 5);
-        ProductService productService = new ProductService(mockProductRepo);
-        Product expected = productService.addProduct(productDTO);
+        Product expected = new Product("1", "Updated Name", 20,"Updated Description", "2", 10);
 
         // When
-        Product actual = productService.updateProduct(expected.withAmount(5).withName("Updated Name").withDescription("Updated Description"));
-
-        expected = expected.withAmount(5).withName("Updated Name").withDescription("Updated Description");
-        // Then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void getProductById_shouldReturnProduct() {
-        // Given
-        ProductDTO productDTO = new ProductDTO("Product", 10,"Description", "1", 5);
-        ProductService productService = new ProductService(mockProductRepo);
-        Product expected = productService.addProduct(productDTO);
-
-        // When
-        Product actual = productService.getProductById(expected.id());
+        when(mockProductRepo.save(expected)).thenReturn(expected);
+        Product actual = productService.updateProduct(expected);
 
         // Then
         assertEquals(expected, actual);
+        verify(mockProductRepo).save(expected);
+        verifyNoMoreInteractions(mockProductRepo);
     }
+
 
     @Test
     void deleteProductById_whenNoSuchProduct_thenThrow() {
         // Given
-        ProductService productService = new ProductService(mockProductRepo);
-
+        String id = "1";
         // When
+        when(mockProductRepo.existsById(id)).thenReturn(false);
         // Then
-        assertThrows(NoSuchProductException.class, () -> productService.deleteProductById("1"));
+        assertThrows(NoSuchProductException.class, () -> productService.deleteProductById(id));
+        verify(mockProductRepo).existsById(id);
+        verifyNoMoreInteractions(mockProductRepo);
     }
 
     @Test
     void deleteProductById_whenSuchProduct_thenDelete(){
         // Given
-        ProductDTO productDTO = new ProductDTO("Product", 10,"Description", "1", 5);
-        ProductService productService = new ProductService(mockProductRepo);
-        Product expected = productService.addProduct(productDTO);
+        String id = "1";
 
         // When
-        productService.deleteProductById(expected.id());
+        when(mockProductRepo.existsById(id)).thenReturn(true);
+        productService.deleteProductById(id);
 
         // Then
-        assertFalse(mockProductRepo.existsById(expected.id()));
+        verify(mockProductRepo).existsById(id);
+        verify(mockProductRepo).deleteById(id);
+        verifyNoMoreInteractions(mockProductRepo);
     }
 
     @Test
     void getProductsInCriticalStock_shouldReturnEmptyList() {
         // Given
-        ProductDTO productDTO = new ProductDTO("Product", 10,"Description", "1", 5);
-        ProductService productService = new ProductService(mockProductRepo);
-        productService.addProduct(productDTO);
-
+        List<Product> allProducts = List.of(
+                new Product("1", "Product 1", 10,"Description 1", "1", 5),
+                new Product("2", "Product 2", 20,"Description 2", "2", 10)
+        );
+        List<Product> expected = List.of();
         // When
+        when(mockProductRepo.findAll()).thenReturn(allProducts);
+        List<Product> actual = productService.getProductsInCriticalStock();
         // Then
-        assertEquals(0, productService.getProductsInCriticalStock().size());
+        assertEquals(expected, actual);
+        verify(mockProductRepo).findAll();
+        verifyNoMoreInteractions(mockProductRepo);
     }
 
     @Test
     void getProductsInCriticalStock_shouldReturnListWithOneProduct() {
         // Given
-        ProductDTO productDTO = new ProductDTO("Product", 10,"Description", "1", 15);
-        ProductService productService = new ProductService(mockProductRepo);
-        productService.addProduct(productDTO);
-
+        Product product01 = new Product("1", "Product 1", 10,"Description 1", "1", 15);
+        Product product02 = new Product("2", "Product 2", 20,"Description 2", "2", 10);
+        List<Product> allProducts = List.of(product01, product02);
+        List<Product> expected = List.of(product01);
         // When
+        when(mockProductRepo.findAll()).thenReturn(allProducts);
+        List<Product> actual = productService.getProductsInCriticalStock();
         // Then
-        assertEquals(1, productService.getProductsInCriticalStock().size());
+        assertEquals(expected, actual);
+        verify(mockProductRepo).findAll();
+        verifyNoMoreInteractions(mockProductRepo);
     }
 }
