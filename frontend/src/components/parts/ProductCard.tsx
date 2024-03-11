5bf2fca8c8aa154e02b9647a4f90ed8205c1d232
Martin <okapiposter@gmail.com>
2024-03-11 20:03:29 +0100

    remove toggle and add hover on ProductCard in .css

--- frontend/src/components/parts/ProductCard.tsx
+++ frontend/src/components/parts/ProductCard.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Product} from '../../types/Product.ts';
 import {Link} from "react-router-dom";
 import './ProductCard.css';
@@ -8,25 +8,13 @@ type ProductCardProps = {
 };
 
 export default function ProductCard({ product }: Readonly<ProductCardProps>): React.ReactElement {
-    const [showMore, setShowMore] = useState(false);
-
-    const toggleDescription = () => {
-        setShowMore(!showMore);
-    };
 
     return (
-
         <div className={"productCard"}>
             <h2>Produkt: {product.name}</h2>
             <p>Artikelnummer: {product.productNumber}</p>
             <p>Menge: {product.amount}</p>
-            {showMore &&
-                <>
-                    <p>Beschreibung: {product.description}</p>
-                    <p><Link to={"/products/" + product.id}>Details</Link></p>
-                </>
-            }
-            <button onClick={toggleDescription}>{showMore ? 'Weniger' : 'Mehr'}</button>
+            <p><Link to={"/products/" + product.id}>Details</Link></p>
         </div>
     );
 }
