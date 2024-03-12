19c2e8dd3d56190cf679fc493326651104a975a5
MaHambach <ma.hambach@gmx.net>
2024-03-12 17:31:09 +0100

    ChangeLog.tsx displays a list of ChangeCards.

--- frontend/src/components/parts/ChangeCard.tsx
+++ frontend/src/components/parts/ChangeCard.tsx
@@ -7,9 +7,11 @@ type ChangeCardProps = {
 export default function ChangeCard(props:Readonly<ChangeCardProps>) {
     return (
       <div className={"changeCard"}>
-          <h3>{props.change.id}</h3>
           <p>{props.change.date}</p>
+          <p>{props.change.productId}</p>
           <p>{props.change.description}</p>
+          <p>{props.change.type}</p>
+          <p>{props.change.status}</p>
       </div>
     );
 }
