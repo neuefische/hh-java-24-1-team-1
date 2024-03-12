16e1405c5a6ae224b2c6206d5e6e3f1de1ec4d2e
MaHambach <ma.hambach@gmx.net>
2024-03-12 14:56:59 +0100

    add imports to ChangeRepo

--- backend/src/main/java/com/github/hh/backend/repository/ChangeRepo.java
+++ backend/src/main/java/com/github/hh/backend/repository/ChangeRepo.java
@@ -1,5 +1,7 @@
 package com.github.hh.backend.repository;
 
+import com.github.hh.backend.model.Change;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
