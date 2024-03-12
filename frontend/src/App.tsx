d55bb3083ad60d05018a0e16c4fd9575bb3f7e41
MaHambach <ma.hambach@gmx.net>
2024-03-12 17:56:41 +0100

    make sonarLint ignore the useEffect in App.tsx, since it would force an infinit-loop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -36,6 +36,7 @@ export default function App(): React.ReactElement {
             }
         )
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(fetchChanges,[products]);
 
     return (
