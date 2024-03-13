3626deea814b9e63915c23e71685a1eaa371077f
MaHambach <ma.hambach@gmx.net>
2024-03-13 11:32:22 +0100

    add @With annotation to properties status and productId of Change.java.

--- backend/src/main/java/com/github/hh/backend/model/Change.java
+++ backend/src/main/java/com/github/hh/backend/model/Change.java
@@ -1,5 +1,6 @@
 package com.github.hh.backend.model;
 
+import lombok.With;
 import org.springframework.data.annotation.Id;
 
 import java.time.Instant;
@@ -7,9 +8,11 @@ import java.time.Instant;
 public record Change(
         @Id
         String id,
+        @With
         String productId,
         String description,
         ChangeType type,
+        @With
         ChangeStatus status,
         Instant date
 ) {
