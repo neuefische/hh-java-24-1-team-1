4ebc24c8357141fecf9c4ada5efccdb7fd67a3c1
MaHambach <ma.hambach@gmx.net>
2024-03-13 11:29:19 +0100

    remove @With annotation from Change.

--- backend/src/main/java/com/github/hh/backend/model/Change.java
+++ backend/src/main/java/com/github/hh/backend/model/Change.java
@@ -1,11 +1,9 @@
 package com.github.hh.backend.model;
 
-import lombok.With;
 import org.springframework.data.annotation.Id;
 
 import java.time.Instant;
 
-@With
 public record Change(
         @Id
         String id,
