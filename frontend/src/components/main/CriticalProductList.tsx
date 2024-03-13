bb0a156c33639f06854a93b6952602db9078b238
Khorm247 <44656481+Khorm247@users.noreply.github.com>
2024-03-13 11:50:08 +0100

    added same css from ProductList.tsx to CriticalProductList.tsx

--- frontend/src/components/main/CriticalProductList.tsx
+++ frontend/src/components/main/CriticalProductList.tsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 import {Product} from "../../types/Product.ts";
 import SearchBar from "../utility/SearchBar.tsx";
 import ProductCard from "../parts/ProductCard.tsx";
+import './ProductList.css';
 
 type CriticalProductListProps = {
     products: Product[];
@@ -13,19 +14,35 @@ function CriticalProductList(props:Readonly<CriticalProductListProps>):JSX.Eleme
     const [searchResults, setSearchResults] = useState<Product[]>();
 
     return (
-        <main className={"criticalProductList"}>
-            <SearchBar handleFilterChange={props.handleFilterChange} handleSearchText={props.handleSearchText} setResult={setSearchResults} products={props.products}/>
-            {
-                searchResults ?
-                    searchResults
-                        .filter((product:Product) => product.minimumStockLevel > product.amount)
-                        .map((product: Product) => (<ProductCard key={product.id} product={product}/>)) :
-                    props.products
-                        .filter((product:Product) => product.minimumStockLevel > product.amount)
-                        .map((product: Product) => (<ProductCard key={product.id} product={product}/>))
-            }
-        </main>
-    );
+        <>
+            <SearchBar handleFilterChange={props.handleFilterChange} handleSearchText={props.handleSearchText}
+                       setResult={setSearchResults} products={props.products}/>
+            <main  className={"productList"}>
+                <table className={"productListTable"}>
+                    <thead>
+                    <tr>
+                        <th>Produktname</th>
+                        <th>Artikelnummer</th>
+                        <th>Anzahl auf Lager</th>
+                        <th>Details</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        searchResults ?
+                            searchResults
+                                .filter((product: Product) => product.minimumStockLevel > product.amount)
+                                .map((product: Product) => (<ProductCard key={product.id} product={product}/>)) :
+                            props.products
+                                .filter((product: Product) => product.minimumStockLevel > product.amount)
+                                .map((product: Product) => (<ProductCard key={product.id} product={product}/>))
+                    }
+                    </tbody>
+                </table>
+            </main>
+        </>
+
+);
 }
 
 export default CriticalProductList;
