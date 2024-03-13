265bcfd0a854bcdaadb76de14a4d7f6a72194a0f
MaHambach <ma.hambach@gmx.net>
2024-03-13 16:50:34 +0100

    fix GlobalExceptionHandler making it a @RestControllerAdvice
    remove apiPath and HttpStatus from ErrorMessage.

--- backend/src/main/java/com/github/hh/backend/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/github/hh/backend/handler/GlobalExceptionHandler.java
@@ -4,39 +4,31 @@ import com.github.hh.backend.exception.NoSuchProductChangeException;
 import com.github.hh.backend.exception.NoSuchProductException;
 import com.github.hh.backend.model.ErrorMessage;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({NoSuchProductException.class})
-    public ResponseEntity<ErrorMessage> handleNoSuchProductException(NoSuchProductException exception,
-                                                                     WebRequest webRequest){
-        ErrorMessage errorMsg = new ErrorMessage(
-                webRequest.getDescription(false),
-                HttpStatus.BAD_REQUEST,
+    @ExceptionHandler(NoSuchProductException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleNoSuchProductException(NoSuchProductException exception){
+        return new ErrorMessage(
                 "Product with ID " + exception.getMessage() + " does not exist",
                 LocalDateTime.now()
         );
-
-        return new ResponseEntity<>(errorMsg, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler({NoSuchProductChangeException.class})
-    public ResponseEntity<ErrorMessage> handleNoSuchChangeException(NoSuchProductChangeException exception,
-                                                                    WebRequest webRequest){
-        ErrorMessage errorMsg = new ErrorMessage(
-                webRequest.getDescription(false),
-                HttpStatus.INTERNAL_SERVER_ERROR,
+    @ExceptionHandler(NoSuchProductChangeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorMessage handleNoSuchChangeException(NoSuchProductChangeException exception){
+        return new ErrorMessage(
                 "Change with ID " + exception.getMessage() + " does not exist. How did you get this message? Seriously, how?",
                 LocalDateTime.now()
         );
-
-        return new ResponseEntity<>(errorMsg, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 }
--- backend/src/main/java/com/github/hh/backend/model/ErrorMessage.java
+++ backend/src/main/java/com/github/hh/backend/model/ErrorMessage.java
@@ -1,12 +1,8 @@
 package com.github.hh.backend.model;
 
-import org.springframework.http.HttpStatus;
-
 import java.time.LocalDateTime;
 
 public record ErrorMessage(
-        String apiPath,
-        HttpStatus errorCode,
         String errorMsg,
         LocalDateTime errorTime
 ) {
