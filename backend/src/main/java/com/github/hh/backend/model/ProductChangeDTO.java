0d113a1c8b2e107c0654b70c337f17669a72b898
MaHambach <ma.hambach@gmx.net>
2024-03-14 20:50:39 +0100

    add equals from ProductChange since it is needed for the tests.
    dito for ProductChangeDTO.

--- backend/src/main/java/com/github/hh/backend/model/ProductChangeDTO.java
+++ backend/src/main/java/com/github/hh/backend/model/ProductChangeDTO.java
@@ -1,6 +1,8 @@
 package com.github.hh.backend.model;
 
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.Objects;
 
 public record ProductChangeDTO(
         Product[] products,
@@ -9,4 +11,11 @@ public record ProductChangeDTO(
         ProductChangeStatus status,
         Instant date
 ) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductChangeDTO that = (ProductChangeDTO) o;
+        return Arrays.equals(products, that.products) && Objects.equals(description, that.description) && type == that.type && status == that.status && Objects.equals(date, that.date);
+    }
 }
