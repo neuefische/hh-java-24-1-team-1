9c800b3bd85de17ed2df3b151eff576d34a0864f
MaHambach <ma.hambach@gmx.net>
2024-03-15 18:26:36 +0100

    fixing diverse problems.

--- frontend/src/components/storageSpace/main/StorageSpaceGallery.tsx
+++ frontend/src/components/storageSpace/main/StorageSpaceGallery.tsx
@@ -1,6 +1,6 @@
 import {StorageSpace} from "../../../types/StorageSpace.ts";
-import {useEffect, useState} from "react";
 import StorageSpaceCard from "../parts/StorageSpaceCard.tsx";
+import React from "react";
 
 
 type StorageSpaceGalleryProps = {
@@ -9,13 +9,7 @@ type StorageSpaceGalleryProps = {
     getOccupiedStorageSpaces: () => StorageSpace[];
     getOccupiedStorageSpacesCount: () => number;
 }
-export default function StorageSpaceGallery(props:Readonly<StorageSpaceGalleryProps>){
-    const [storageSpaces, setStorageSpaces] = useState<StorageSpace[]>(props.storageSpaces);
-
-    useEffect(() => {
-        setStorageSpaces(props.storageSpaces);
-    }, []);
-
+export default function StorageSpaceGallery(props:Readonly<StorageSpaceGalleryProps>):React.ReactElement{
     return (
         <main className={"storageSpaceGallery"}>
             <div>
@@ -32,9 +26,8 @@ export default function StorageSpaceGallery(props:Readonly<StorageSpaceGalleryPr
                 </thead>
                 <tbody>
                 {
-                    storageSpaces ?
-                        storageSpaces.map((space:StorageSpace) => (<StorageSpaceCard key={space.storageSpaceName} space={space}/>)) :
-                        props.storageSpaces.map((space:StorageSpace) => (<StorageSpaceCard key={space.storageSpaceName} space={space}/>))
+
+                        props.storageSpaces.map((space:StorageSpace) => (<StorageSpaceCard key={space.id} space={space}/>))
                 }
                 </tbody>
             </table>
