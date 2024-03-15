bb65582352986a471c07cf9d5d15feab2999d157
MaHambach <ma.hambach@gmx.net>
2024-03-15 11:30:44 +0100

    change array in productChangeDTO to list.

--- backend/src/main/java/com/github/hh/backend/model/ProductChangeDTO.java
+++ backend/src/main/java/com/github/hh/backend/model/ProductChangeDTO.java
@@ -1,21 +1,13 @@
 package com.github.hh.backend.model;
 
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.List;
 
 public record ProductChangeDTO(
-        Product[] products,
+        List<Product> products,
         String description,
         ProductChangeType type,
         ProductChangeStatus status,
         Instant date
 ) {
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ProductChangeDTO that = (ProductChangeDTO) o;
-        return Arrays.equals(products, that.products) && Objects.equals(description, that.description) && type == that.type && status == that.status && Objects.equals(date, that.date);
-    }
 }
