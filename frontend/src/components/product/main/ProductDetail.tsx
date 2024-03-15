ccb3b2f0164c8cd46379c785cbacf7d6b97a3525
MaHambach <ma.hambach@gmx.net>
2024-03-15 16:27:25 +0100

    move all main components into folder components/product/main
    move all part components into folder components/product/parts

--- frontend/src/components/product/main/ProductDetail.tsx
+++ frontend/src/components/product/main/ProductDetail.tsx
@@ -2,7 +2,7 @@ import './ProductDetail.css'
 import {useEffect, useState} from "react";
 import {NavigateFunction, useNavigate, useParams} from "react-router-dom";
 import {Product} from "../../../types/Product.ts";
-import Barcode from "../../parts/Barcode.tsx";
+import Barcode from "../parts/Barcode.tsx";
 
 type ProductDetailProps = {
     getProductById:(id:string)=>Product,
