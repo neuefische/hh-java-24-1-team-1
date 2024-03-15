9de447d47f6dc2cd9e154b0ca49558c205045f95
MaHambach <ma.hambach@gmx.net>
2024-03-15 18:31:10 +0100

    add link to storage page in Header.tsx

--- frontend/src/components/utility/Header.tsx
+++ frontend/src/components/utility/Header.tsx
@@ -13,6 +13,7 @@ export default function Header(props: Readonly<HeaderProps>){
             <Link className={"link"} to={"/products/add"}>Produkt Hinzufügen</Link>
             <Link className={"link"} to={"/critical"} onClick={() => props.criticalHook()}>Kritische Produkte</Link>
             <Link className={"link"} to={"/products/changelog"} onClick={() => props.criticalHook()}>Changelog</Link>
+            <Link className={"link"} to={"/storage"} onClick={() => props.criticalHook()}>Lagerplätze</Link>
         </header>
     )
 }
