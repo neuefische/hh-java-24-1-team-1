8826e8776610ffb48d0d999572e393820f44e641
MaHambach <ma.hambach@gmx.net>
2024-03-16 22:48:00 +0100

    fix some rebase problems.
    add products to the storage spaces they occupy in StorageSpaceGallery.tsx

--- frontend/src/components/storageSpace/parts/StorageSpaceCard.tsx
+++ frontend/src/components/storageSpace/parts/StorageSpaceCard.tsx
@@ -1,15 +1,19 @@
 import {StorageSpace} from "../../../types/StorageSpace.ts";
 import React from "react";
+import {Product} from "../../../types/Product.ts";
+import ProductCard from "../../product/parts/ProductCard.tsx";
 
 type StorageSpaceCardProps = {
     space: StorageSpace;
+    product: Product | null;
 }
 
 export default function StorageSpaceCard(props: Readonly<StorageSpaceCardProps>): React.ReactElement {
     return (
         <tr className={"storageSpaceCard"}>
             <td><p>{props.space.storageSpaceName}</p></td>
-            <td><p>{props.space.isOccupied ? "Ja" : "Nein"}</p></td>
+            <td>{props.space.isOccupied ? <p className={"yesDiv"}>"Ja"</p> : <p className={"noDiv"}>"Nein"</p>}</td>
+            <td><p>{props.product ? <ProductCard product={props.product}/> : "Kein Produkt"}</p></td>
         </tr>
     );
 }
