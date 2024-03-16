13b040c58a2ec6e24f199a9f06ca2f5c9bf9a21e
MaHambach <ma.hambach@gmx.net>
2024-03-17 00:17:37 +0100

    add some styling to StorageSpaceCard.tsx add some styling to StorageSpaceGallery.tsx

--- frontend/src/components/storageSpace/main/StorageSpaceGallery.tsx
+++ frontend/src/components/storageSpace/main/StorageSpaceGallery.tsx
@@ -1,5 +1,6 @@
+import './StorageSpaceGallery.css';
 import {StorageSpace} from "../../../types/StorageSpace.ts";
-import StorageSpaceCard from "../parts/StorageSpaceCard.tsx";
+import StorageSpaceCard from '../parts/StorageSpaceCard.tsx';
 import React from "react";
 import {Product} from "../../../types/Product.ts";
 
@@ -20,21 +21,33 @@ export default function StorageSpaceGallery(props:Readonly<StorageSpaceGalleryPr
                 <p>Belegte Lagerplätze: {props.getOccupiedStorageSpacesCount()}</p>
                 <p>Freie Lagerplätze: {props.getEmptyStorageSpaces().length}</p>
             </div>
-            <table>
+            <table className={"storageSpaceGalleryTable"}>
                 <thead>
                     <tr>
                         <th>Lagerplatznummer</th>
-                        <th>Belegt?</th>
+                        <th>Belegung</th>
+                        <th className={"productHeadColumn"}>
+                            <table className={"productHeadTable"}>
+                                <thead className={"productHeadTbody"}>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Artikel Nr.</th>
+                                    <th>Auflager</th>
+                                    <th>Details</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
                 {
-                        props.storageSpaces.map((space:StorageSpace) =>
-                            (<StorageSpaceCard
-                                key={space.id}
-                                space={space}
-                                product={props.products.find((product:Product) => product.storageSpaceName === space.storageSpaceName) ?? null}
-                            />))
+                    props.storageSpaces.map((space: StorageSpace) =>
+                        (<StorageSpaceCard
+                            key={space.id}
+                            space={space}
+                            product={props.products.find((product: Product) => product.storageSpaceName === space.storageSpaceName) ?? null}
+                        />))
                 }
                 </tbody>
             </table>
--- frontend/src/components/storageSpace/parts/StorageSpaceCard.tsx
+++ frontend/src/components/storageSpace/parts/StorageSpaceCard.tsx
@@ -1,7 +1,8 @@
+import './StorageSpaceCard.css';
 import {StorageSpace} from "../../../types/StorageSpace.ts";
 import React from "react";
 import {Product} from "../../../types/Product.ts";
-import ProductCard from "../../product/parts/ProductCard.tsx";
+import {Link} from "react-router-dom";
 
 type StorageSpaceCardProps = {
     space: StorageSpace;
@@ -12,8 +13,31 @@ export default function StorageSpaceCard(props: Readonly<StorageSpaceCardProps>)
     return (
         <tr className={"storageSpaceCard"}>
             <td><p>{props.space.storageSpaceName}</p></td>
-            <td>{props.space.isOccupied ? <p className={"yesDiv"}>"Ja"</p> : <p className={"noDiv"}>"Nein"</p>}</td>
-            <td><p>{props.product ? <ProductCard product={props.product}/> : "Kein Produkt"}</p></td>
+            {props.space.isOccupied ?
+                <td className={"yesDiv"}><p>Belegt</p> </td>:
+                <td className={"noDiv"}><p>Frei</p></td>
+            }
+            <td className={"productColumn"}>
+                {props.product ?
+                    <table className={"productTable"}>
+                        <tbody className={"productTbody"}>
+                        <tr>
+                            <td><p><b>{props.product.name}</b></p></td>
+                            <td><p>{props.product.productNumber}</p></td>
+                            <td><p>{props.product.amount}</p></td>
+                            <td><p><Link to={"/products/" + props.product.id}>Details</Link></p></td>
+                        </tr>
+                        </tbody>
+                    </table> :
+                    <table className={"productTable"}>
+                        <tbody>
+                        <tr>
+                            <td><p>"Kein Produkt"</p></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                }
+            </td>
         </tr>
     );
 }
