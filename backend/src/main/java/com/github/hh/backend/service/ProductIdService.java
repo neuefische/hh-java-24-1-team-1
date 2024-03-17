0ef8cdef57cef4e4815db36ca8a28d05a016b5e4
MaHambach <ma.hambach@gmx.net>
2024-03-17 18:25:14 +0100

    change used id in ProductIdService to "GenericProductIdCounter"

--- backend/src/main/java/com/github/hh/backend/service/ProductIdService.java
+++ backend/src/main/java/com/github/hh/backend/service/ProductIdService.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Service;
 public class ProductIdService {
     private final ProductIdRepo productIdRepo;
     public String generateProductId() {
-        int newProductId = productIdRepo.findById("65f3236ac6ef8b96fa359b47").orElse(new ProductId("no", 0)).productId() + 1;
-        productIdRepo.save(new ProductId("65f3236ac6ef8b96fa359b47", newProductId));
+        int newProductId = productIdRepo.findById("GenericProductIdCounter").orElse(new ProductId("no", 0)).productId() + 1;
+        productIdRepo.save(new ProductId("GenericProductIdCounter", newProductId));
         return "P" + newProductId;
     }
 }
